fd9223ed0ad1b06661679a57745b17ebd9031053
Patrick Dutra <patrickdutra100@hotmail.com>
2023-04-04 11:59:22 -0300

    arrow fc

---
+++ script.js
@@ -0,0 +1,12 @@
+// function soma(a, b) {
+//     return a + b;
+// }
+// console.log(soma(3, 3));
+
+// Arrow Function
+// soma = (a, b) => a + b;
+// console.log(soma(1, 7));
+
+// Arrow Function With Parentheses:
+// hello = (val) => "Hello " + val;
+// console.log(hello("Universe!"));
